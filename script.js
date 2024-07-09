188098b93f43e992404284d8fd962371d048a543
Mouzone <sunnyliu010@gmail.com>
2024-07-09 11:01:57 -0400

    Change "Read?" to a toggle that changes the state in the data structure as well

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// todo: make all input fields mandatory before submitting
+// todo: add sorting for each column
+// todo: style everything better (specifically mobile view
+// todo: popup for duplicates in title and author
+
 const myLibrary = {}
 
 function Book(title, author, pages, hasRead) {
@@ -8,7 +13,6 @@ function Book(title, author, pages, hasRead) {
 }
 
 function addBookToLibrary(book) {
-    const table = document.getElementById("library")
     const table_body = document.querySelector("tbody")
     const tr = document.createElement("tr")
     table_body.insertAdjacentElement("beforeend", tr)
@@ -16,19 +20,30 @@ function addBookToLibrary(book) {
     tr.insertAdjacentHTML("beforeend",
         `<td> ${book.title} </td>
               <td> ${book.author} </td>
-              <td> ${book.pages} </td>
-              <td> ${book.hasRead} </td>`)
+              <td> ${book.pages} </td>`)
+
+    const has_read_toggle_cell = document.createElement("td")
+    tr.insertAdjacentElement("beforeend", has_read_toggle_cell)
+    const has_read_toggle = document.createElement("input");
+    has_read_toggle_cell.insertAdjacentElement("beforeend", has_read_toggle)
+    has_read_toggle.setAttribute("type", "checkbox")
+    has_read_toggle.checked = book.hasRead
+    has_read_toggle.dataset.id = `${counter}`
+    has_read_toggle.addEventListener("change", event => {
+        myLibrary[has_read_toggle.dataset.id].hasRead = !myLibrary[has_read_toggle.dataset.id].hasRead
+        console.log(myLibrary)
+    })
+
     const remove_button_cell = document.createElement("td")
     tr.insertAdjacentElement("beforeend", remove_button_cell)
     const remove_button = document.createElement("button")
     remove_button_cell.insertAdjacentElement("beforeend", remove_button)
     remove_button.classList.add("remove")
-    remove_button.dataset.row = `${counter}`
+    remove_button.dataset.id = `${counter}`
     remove_button.textContent = "Remove"
     remove_button.addEventListener("click", event => {
-        // fix this section
-        const row_to_remove = document.querySelector(`tr[data-row="${remove_button.dataset.row}"]`)
-        delete myLibrary[remove_button.dataset.row]
+        const row_to_remove = document.querySelector(`tr[data-row="${remove_button.dataset.id}"]`)
+        delete myLibrary[remove_button.dataset.id]
         row_to_remove.remove()
     })
 
